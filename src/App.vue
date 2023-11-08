7ea1f652d9fd001c574a0ec0ae0504027d947155
unknown <lawrenciaebube@gmail.com>
2023-11-08 14:46:44 +0100

    just laptopview

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!-- <header> 
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+   </header>  -->
+  
+
+  <RouterView />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
